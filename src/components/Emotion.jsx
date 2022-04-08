4b7cea0e7661fdad5bf444ad6337ea605ace675e
tssmm1 <102934326+tssmm1@users.noreply.github.com>
2022-04-08 17:17:32 +0900

    inline-finish

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,6 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
+import styled from "@emotion/styled";
 
 export const Emotion = () => {
   const containerStyle = css`
@@ -12,10 +13,26 @@ export const Emotion = () => {
     justify-content: space-around;
     align-items: center;
   `;
+  const titleStyle = css({
+    margin: 0,
+    color: "#3d84a8"
+  });
   return (
     <div css={containerStyle}>
-      <p>- Emotion -</p>
-      <button>FIGHT!!</button>
+      <p css={titleStyle}>- Emotion -</p>
+      <SButton>FIGHT!!</SButton>
     </div>
   );
 };
+
+const SButton = styled.button`
+  background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
